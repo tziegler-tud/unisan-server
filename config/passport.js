ba14d1ea3e6f97cbcc99c6b07701ed30e055ef55
Louis Wilke <louiswilke@gmx.de>
2019-12-11 16:12:08 +0100

    made the login method async for performance

--- config/passport.js
+++ config/passport.js
@@ -3,40 +3,36 @@ const LocalStrategy =   require('passport-local').Strategy;
 const bcrypt =          require ('bcryptjs');
 const userService = require('../services/userService');
 
-var salt = bcrypt.genSaltSync(10);
-var hash = bcrypt.hashSync("password", salt);
+passport.use(
 
-const users = [
-    {id: '2f24vvg', name: 'test', hash: hash}
-];
+    // @louis I made the login function async because of performance reasons. An attacker could spam the login and this would block the server due to a synchronous (blocking) bcrypt call!
+    new LocalStrategy(
+        {
+            usernameField: 'username',
+            passwordField: 'password',
+        }, async (name, password, done) => {
 
-passport.use(new LocalStrategy({
-    usernameField: 'username',
-    passwordField: 'password',
-}, (name, password, done) => {
-    console.log('Inside local strategy callback');
-        // here is where you make a call to the database
-        // to find the user based on their username or email address
-        // for now, we'll just pretend we found that it was users[0]
+            const user = await userService.getByUsername(name);
+            // TODO: When done with debugging we should remove this message as any user can now try out random usernames until he finally gets the message "wrong password". This is a security risk!
+            if (user == null) {
+                console.log("Username not found. :-(");
+                return done(null, false, { message: "No user with the given username exists" });
+            }
+            
+            // check the password
+            if(name === user._doc.username && await bcrypt.compare(password, user._doc.hash)) {
+                console.log('password ok');
+                return done(null, user);
+            } else {
+                console.log('login failed');
+                return done(null, false, { message: "The given password does not match the username" });
 
-        userService.getByUsername(name)
-            .then(user => user ? function (){
-                if(name === user._doc.username && bcrypt.compareSync(password, user._doc.hash)) {
-                    console.log('password ok');
-                    return done(null, user)
-                }
-                else {
-                    console.log('login failed');
-                    return done(null,false);
+            }
 
-                }
-            }() : function(){
-                console.log("username not found. :-(");
-                return done(null,false);
-            }())
-            .catch(err => console.log(err))
+        }
+    )
 
-}));
+);
 
 // tell passport how to serialize the user
 passport.serializeUser((user, done) => {
