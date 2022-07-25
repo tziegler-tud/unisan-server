2d769669993cb6a17ea302a4f39a6b94912ce335
Tom Ziegler <tom.ziegler1@gmx.de>
2022-07-25 13:43:24 +0200

    removed some obsolete files

--- src/webpack/user/userLogs.js
+++ src/webpack/user/userLogs.js
@@ -2,6 +2,7 @@ import {Sidebar, SidebarPlugin, ContentHandler} from "../sidebar/sidebar.js";
 import {userPlugin} from "../sidebar/plugins/plugin-user";
 import {UserProfile} from "../userprofile/userprofile";
 import {userActions} from "../actions/userActions";
+import {ScrollableList} from "../scrollableList/scrollableList"
 
 import {lidl} from "/src/lib/lidl-modules/core/lidlModular-0.2";
 import {Observer as lidlObserver} from "/src/lib/lidl-modules/observer/lidl-observer";
@@ -70,76 +71,9 @@ $(document).ready (function () {
                         }
                     }
                 }
-                let scrollableList = new common.ScrollableList(container, "log", result, args, callback)
+                let scrollableList = new ScrollableList(container, "log", result, args, callback)
                 // displayLogList(result);
             }
         })
-        // function displayLogList(result) {
-        //
-        //     var handleData = {logs: result};
-        //
-        //     // render participantslist template
-        //     $.get('/static/unisams/js/userprofile/templates/loglist.hbs', function (data) {
-        //         var template = Handlebars.compile(data);
-        //         appendContent(template(handleData))
-        //     });
-        //
-        //     function appendContent(html) {
-        //         //append to subpage container #userlist-container
-        //         let container = document.getElementById('loglist-container');
-        //         container.innerHTML = html;
-        //
-        //         //delay adjustment until sidebar finished rendering
-        //         setTimeout(function(){
-        //          adjustList(container)
-        //         }, 250);
-        //         // hook log entries to sidebar.
-        //         $('.log-entry').each(function () {
-        //             $(this).on("click", function (e) {
-        //                 let self = e.currentTarget;
-        //                 e.preventDefault();
-        //                 //get full log from results
-        //                 let logObj = result.find(function(el){
-        //                     return el.id === self.dataset.logid;
-        //                 })
-        //                 sidebar.addContent("logDetails", {
-        //                     log: logObj
-        //                 });
-        //                 sidebar.show();
-        //             })
-        //         });
-        //
-        //
-        //     }
-        //     function adjustList(c) {
-        //         let container = c === undefined ? document.getElementById('loglist-container') : c;
-        //         // set width of first row
-        //         let row = document.getElementById("loglist-top");
-        //
-        //         //get viewport height
-        //         const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-        //         //get height of top navigation and topbar element
-        //         let navHeight = document.getElementById("nav-top").clientHeight +1;
-        //         let topbarHeight = document.getElementById("content1-heading").clientHeight;
-        //         //calc remaining height
-        //         let h = vh - (navHeight + topbarHeight + row.clientHeight + 10);
-        //         //set element height
-        //         $(container).css({
-        //             "height": h + "px",
-        //             "overflow": "auto",
-        //         });
-        //
-        //         //get scollbar width
-        //         let scrollbarWidth = container.offsetWidth - container.clientWidth;
-        //         $(row).css({
-        //             "width": "auto",
-        //             "padding-right": scrollbarWidth + "px",
-        //         })
-        //     }
-        //
-        //     $(window).on('resize',function(){
-        //         adjustList();
-        //     });
-        // }
     }
 });
