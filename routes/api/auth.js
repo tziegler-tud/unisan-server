d98e0436ccce295abb8a5329cfb776b85d8bb179
Tom Ziegler <tom.ziegler1@gmx.de>
2023-02-01 13:55:44 +0100

    small fix to api auth endpoint to return json instead of string

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -16,9 +16,9 @@ app.use(bodyParser.json());
 router.post('/auth', function(req, res, next) {
     console.log('Inside POST /login callback');
     passport.authenticate('local', {}, (err, user, info) => {
-        if (!user) { return res.status(401).send("login failed."); }
+        if (!user) { return res.status(401).json({response: "login failed."}); }
         req.login(user, (err) => {
-            res.status(200).send("Authentication successful.");
+            res.status(200).json(user);
         })
     })(req, res, next);
 });
