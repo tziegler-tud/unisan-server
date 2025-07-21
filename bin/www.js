3172e40f4e3a3ee2d224fb641f7f6ad916b522e6
Tom Ziegler <tom.ziegler1@gmx.de>
2025-07-21 17:17:10 +0200

    updated node dependencies, moved to node 22

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import config from "../config/config.json" assert {type: "json"};
+import config from "../config/config.json" with {type: "json"};
 
 /**
  * Module dependencies.
